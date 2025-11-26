87439785f453eb6fb0cd766255766eb0d525ec99
tomasf12 <tomas.fickert@gmail.com>
2025-11-25 19:03:13 -0500

    Removed Unnessecary Comments

--- a5_skeleton.c
+++ a5_skeleton.c
@@ -1,20 +1,4 @@
-/*
-Student Name: Tomas Fickert
-Student ID: 1295844
-Course: CIS*1300 | Assignment 5
-Due Date: November 25th, at 11:59pm
 
-By including this statement in this header comment, I certify that I have read and understood the University policy on academic integrity.
-
-I assert that this work is 100% my own. 
-I assert that I did not copy code from another person.
-I assert that I did not copy code from AI.
-I understand that I will receive a grade of 0 if I copy code from another person or from AI, as this constitutes academic dishonesty.
-
-I have appropriately acknowledged all materials (code, data, images, ideas or words) that I have used, whether directly quoted or paraphrased.
-
-Furthermore, I certify that this assignment was prepared by me specifically for the CIS*1300 Fall 2025 course.
-*/  
 
 #include "a5.h"
 
